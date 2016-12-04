9c0accac3e4ad2b59d398a1c1ba8fb2897600675
sugyan <sugi1982@gmail.com>
2016-12-04 23:47:46 +0900

    update

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def main(argv=None):
     classify_image.create_graph()
     with tf.Session() as sess:
         pool3 = sess.graph.get_tensor_by_name('pool_3:0')
+        outputs = []
         for filename in os.listdir('images'):
             if not filename.endswith('.JPG'):
                 continue
             with open(os.path.join('images', filename), 'rb') as f:
-                output = sess.run(pool3, {'DecodeJpeg/contents:0': f.read()})
-            print(output.shape)
+                outputs.append(sess.run(pool3, {'DecodeJpeg/contents:0': f.read()}))
+        embedding_var = tf.Variable(tf.pack([tf.squeeze(x) for x in outputs], axis=0), trainable=False)
+        # save embedding_var
+        sess.run(tf.variables_initializer([embedding_var]))
+        saver = tf.train.Saver([embedding_var])
+        saver.save(sess, os.path.join('logdir', 'model.ckpt'))
 
 
 if __name__ == '__main__':
